fdc800891790f0d26537aab44ccf32459f3f58d3
Andrey Merkulov <merkulov.andrej@gmail.com>
2022-05-15 01:04:22 +0200

    Added Category filter layout

--- app/src/main/java/com/iiddd/learnapp/MainActivity.java
+++ app/src/main/java/com/iiddd/learnapp/MainActivity.java
@@ -1,14 +1,43 @@
 package com.iiddd.learnapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.iiddd.learnapp.adapter.CategoryAdapter;
+import com.iiddd.learnapp.model.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView categoryRecycler;
+    CategoryAdapter categoryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, "Игры"));
+        categoryList.add(new Category(2, "Сайты"));
+        categoryList.add(new Category(3, "Языки"));
+        categoryList.add(new Category(4, "Прочее"));
+
+        setCategoryRecycler(categoryList);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
     }
 }
